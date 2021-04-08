7ec9fe3fd5683a5b3b481afc89246e957c3a6b49
danielevergani <dvergani94@gmail.com>
2021-04-08 16:14:36 +0200

    tentativo for each

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 var app = new Vue ({
     el: `#app`,
     data: {
-        albums: []
+        albums: [],
+        generi: null
+    },
+    methods:{
+        albumDispo: function(){
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then(( results ) => {
+                this.albums = results.data.response;
+            });
+        },
+
+        selectConstructor: function(){
+            this.albums.forEach((elemento) => {
+                console.log(elemento);
+            });
+        }
     },
     mounted: function(){
-        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
-        .then(( results ) => {
-            
-            this.albums = results.data.response;
-            console.log( this.albums  );
-        });
+        this.albumDispo();
+        this.selectConstructor();
     }
 });
