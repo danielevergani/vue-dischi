c214ff360c39435f6b71e9dcb063eaeb52d5b3c5
danielevergani <dvergani94@gmail.com>
2021-04-08 16:51:30 +0200

    inserita select funzionante

--- js/script.js
+++ js/script.js
@@ -2,24 +2,30 @@ var app = new Vue ({
     el: `#app`,
     data: {
         albums: [],
-        generi: null
+        genre: ["All"],
+        genreSelected: "All"
     },
     methods:{
         albumDispo: function(){
             axios.get("https://flynn.boolean.careers/exercises/api/array/music")
             .then(( results ) => {
                 this.albums = results.data.response;
+                this.selectConstructor();
             });
         },
 
         selectConstructor: function(){
             this.albums.forEach((elemento) => {
-                console.log(elemento);
+                if ( this.genre.includes(elemento.genre) == false ){
+                    this.genre.push(elemento.genre);
+                }
             });
-        }
+            console.log(this.genre);
+        },
+
     },
+
     mounted: function(){
         this.albumDispo();
-        this.selectConstructor();
     }
 });
