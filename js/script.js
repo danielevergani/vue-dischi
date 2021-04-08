ff6d619ceeddfa11985e3cfa4e90f6569e58362e
danielevergani <dvergani94@gmail.com>
2021-04-08 15:15:19 +0200

    "inizio"

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var app = new Vue ({
+    el: `#app`,
+    data: {
+        numero: "ciao",
+        albums: [
+            {
+                poster: "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+                title: "New Jersey",
+                author: "Bon Jovi",
+                genre: "Rock",
+                year: "1988"
+            },
+        ]
+    }
+});
